cee571f685e7eb68c81df98273a0cf4d92114ee6
dormantgoodness <aiddom107@gmail.com>
2025-03-27 21:34:59 -0500

    Add files via upload

---
+++ canvaslist.cpp
@@ -0,0 +1,7 @@
+#include "canvaslist.h"
+
+#include <iostream>
+
+using namespace std;
+
+// TODO_STUDENT
---
+++ shape.cpp
@@ -0,0 +1,46 @@
+#include "include/shape.h"
+
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+Shape::Shape() {
+  // TODO_STUDENT
+}
+
+Shape::Shape(int x, int y) {
+  // TODO_STUDENT
+}
+
+Shape::~Shape() {
+  // TODO_STUDENT
+}
+
+Shape* Shape::copy() {
+  // TODO_STUDENT
+  return nullptr;
+}
+
+int Shape::getX() const {
+  // TODO_STUDENT
+  return 0;
+}
+
+int Shape::getY() const {
+  // TODO_STUDENT
+  return 0;
+}
+
+void Shape::setX(int x) {
+  // TODO_STUDENT
+}
+
+void Shape::setY(int y) {
+  // TODO_STUDENT
+}
+
+string Shape::as_string() const {
+  // TODO_STUDENT
+  return "";
+}
